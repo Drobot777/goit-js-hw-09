168143949e06f651896fe77a83981e3df121e8c3
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-06-01 13:34:49 +0300

    Done 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startEl = document.querySelector('.data-start')
+const stopEl = document.querySelector('.data-stop')
+stopEl.setAttribute('disabled', true)
+startEl.addEventListener('click', startRandomColor)
 
+
+function startRandomColor() {
+    startEl.setAttribute('disabled', true)
+    stopEl.removeAttribute('disabled')
+    const interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+        stopEl.addEventListener('click', () => {
+            stopEl.setAttribute('disabled', true)
+    startEl.removeAttribute('disabled')
+    clearInterval(interval)
+      } )  
+    }, 1000)
+   
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
